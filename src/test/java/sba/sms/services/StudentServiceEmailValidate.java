68080204d52b1af33d95732ced1ea605b9c52c39
Patrick Tuyiringire <tpatrickrumanzi@gmail.com>
2023-01-30 21:45:31 -0800

    JUnit test case added

---
+++ src/test/java/sba/sms/services/StudentServiceEmailValidate.java
@@ -0,0 +1,35 @@
+package sba.sms.services;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import sba.sms.utils.CommandLine;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+class StudentServiceEmailValidate {
+	static StudentService studentService;
+
+    @BeforeAll
+    static void beforeAll() {
+        studentService = new StudentService();
+        CommandLine.addData();
+    }
+
+	@Test
+	void validateStudent() {
+		String email = "reema@gmail.com";
+		String pass = "password";
+		boolean validateEmail = studentService.validateStudent(email, pass);
+		System.out.println("Done");
+        assertEquals(true, validateEmail);
+	}
+
+}
